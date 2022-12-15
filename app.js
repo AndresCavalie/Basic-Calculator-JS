4825f4b41a6cbcfca4a1a9ba35a399487988e976
AndresCavalie <andrescavalie02@gmail.com>
2022-12-15 17:23:18 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  var img = document.createElement("img");
+
+  img.src = "matplotlib-grid-02.png";
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+    console.log("RUNNING EQUALS");
+  });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+})();
